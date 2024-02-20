0e66a9eb213f648de062cbcb06f2e7b11d4eae72
henrytanglobal <84701140+henrytanglobal@users.noreply.github.com>
2024-02-20 14:24:37 +0100

    updated name

--- src/App.tsx
+++ src/App.tsx
@@ -125,7 +125,7 @@ function App() {
         <div className="App-logo">
           <img src={require('./impic.jpeg')} alt="Logo" style={{ maxWidth: '100px', maxHeight: '100px' }} />
         </div>
-        <h1 className="App-title">Proof of Impact Protocol</h1>
+        <h1 className="App-title">Proof of Impact Protocol by Impactopia</h1>
       </div>
       <header className="App-header">
         <p className="address">
